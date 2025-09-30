6c811a2c0e39c9736de3de7a66cdd24f13f94ff1
KikeyGuan <Kikey.Guan@mail.citytech.cuny.edu>
2025-09-30 03:04:57 -0400

    need help
    coroutine is confusing

--- Assets/script/move.cs
+++ Assets/script/move.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     private Vector3 walkSpot;
+    private Vector3 mousePosition;
     public float coolDown, speed;
     float randX, randY;
-    private IEnumerator coroutine;
+    private IEnumerator coroutineWalkCD, coroutineStayCD;
     public Animator animator;
     public SpriteRenderer sprite;
     public dragable clickBool;
+    public Camera cam;
 
     void Start()
     {
         //pint(Screen.width);
         //print(Screen.height);
 
-        coroutine = walkCoolDown(coolDown);
-        StartCoroutine(coroutine);
+        coroutineWalkCD = walkCoolDown(coolDown);
+        coroutineStayCD = stayCoolDown(coolDown);
+        StartCoroutine(coroutineWalkCD);
+        //StartCoroutine(coroutineStayCD);
+
 
     }
 
@@ -54,11 +59,22 @@ public class move : MonoBehaviour
         if (clickBool.clicked == true) //lift
         {
             animator.SetBool("mouseClick", true);
-
         }
 
 
 
+
+
+        //testing if start and stop coroutine works
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(coroutineStayCD);
+            print("starting up");
+        }
+        else
+        {
+            StopCoroutine(coroutineStayCD);
+        }
     }
 
     void randSpace()
@@ -69,14 +85,38 @@ public class move : MonoBehaviour
 
     private IEnumerator walkCoolDown(float i)
     {
+        print("start WALK");
+        StopCoroutine(coroutineStayCD);
         while (true)
         {
             yield return new WaitForSeconds(i);
-            //print("hello");
+            print("hello");
             randSpace();
         }
 
     }
+
+
     //Need to stop timer, leave "moveTowards" where player drop them off at, then continue timer after some time
+    private IEnumerator stayCoolDown(float i)
+    {
+        print("start STAY");
+        //StopCoroutine(coroutineWalkCD);
+        //mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        //walkSpot = new Vector3(mousePosition.x, mousePosition.y, 0);
+        yield return new WaitForSeconds(i);
+        print(i);
+        StartCoroutine(coroutineWalkCD);
+    }
+
+    void OnMouseUp()
+    {
+        print("mouse up");
+        //StopCoroutine(coroutineWalkCD);
+        StartCoroutine(coroutineStayCD);
+        
+    }
+
+
     
 }
